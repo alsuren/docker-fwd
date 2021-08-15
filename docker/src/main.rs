38d9f00ea2542b9b897b5c8d86bf9a010927623b
David Laban <alsuren@gmail.com>
2021-08-15 20:34:12 +0100

    parse `docker run` and interpret -p 8080:8080 for port forwarding

--- docker/src/main.rs
+++ docker/src/main.rs
@@ -1,3 +1,8 @@
+mod args;
+use std::process::exit;
+
+use argh::FromArgs;
+
 fn main() {
     run().unwrap()
 }
@@ -8,20 +13,44 @@ fn run() -> Result<(), std::io::Error> {
     // Parse -p x:y flags and create corresponding -L x:localhost:y flags
     // Explode if anyone tries to -P (port forward everything that's exposed)
     // Explode if anyone tries to port-forward and also run in the background (-d)
-    let args: Vec<_> = std::env::args().skip(1).collect();
+    let args: Vec<_> = std::env::args().collect();
+    let strs: Vec<&str> = args.iter().map(|s| s.as_str()).collect();
+    let mut ssh_flags: Vec<String> = Vec::new();
+    match args::Docker::from_args(&["docker"], &strs[1..]) {
+        Ok(args::Docker {
+            subcommand: args::Subcommand::Run(run),
+        }) => {
+            if !run.volume.is_empty() {
+                dbg!(&run.volume);
+            }
+            for mapping in run.publish.iter() {
+                let (l, r) = mapping
+                    .split_once(':')
+                    .expect("-p mappings must contain a :");
+                ssh_flags.push(format!("-L{l}:localhost:{r}", l = l, r = r))
+            }
+        }
+        Err(e) => {
+            dbg!(&e);
+            if std::env::var("PEDANTIC").is_ok() {
+                exit(1)
+            }
+        }
+    }
 
     // We map "$PWD" outside the vm to "$HOME/$PWD" inside.
     let pwd = format!("./{}", std::env::current_dir()?.to_str().unwrap());
 
     let mut command = std::process::Command::new("ssh");
     command
+        .args(ssh_flags)
         // ssh -t allocates a tty so that ^C works correctly
         .args(["-t", "localhost"])
         // We assume that https://crates.io/crates/in-directory is installed already.
         .args(["./bin/in", &pwd])
         // Forward the rest of the docker args unchanged
         .arg("docker")
-        .args(args);
+        .args(&args[1..]);
 
     // TODO: switch to pretty_env_logger or something
     eprintln!("Running: {:?}", &command);
@@ -34,6 +63,8 @@ fn run() -> Result<(), std::io::Error> {
         .stdout(std::process::Stdio::inherit())
         .stderr(std::process::Stdio::inherit());
     let mut process = command.spawn()?;
+    // TODO: if `cargo watch -x run` kills us, we don't kill the docker
+    // image properly (^C works fine though).
     process.wait()?;
 
     Ok(())
