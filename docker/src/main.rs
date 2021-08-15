afafc762e32922035726f1d8d1e655352d8ce0a1
David Laban <alsuren@gmail.com>
2021-08-15 17:25:06 +0100

    fix ^C; clean things up a bit

--- docker/src/main.rs
+++ docker/src/main.rs
@@ -6,19 +6,33 @@ fn run() -> Result<(), std::io::Error> {
     // TODO:
     // Parse -v flags, and replace them appropriately
     // Parse -p x:y flags and create corresponding -L x:localhost:y flags
+    // Explode if anyone tries to -P (port forward everything that's exposed)
     // Explode if anyone tries to port-forward and also run in the background (-d)
     let args: Vec<_> = std::env::args().skip(1).collect();
 
+    // We map "$PWD" outside the vm to "$HOME/$PWD" inside.
     let pwd = format!("./{}", std::env::current_dir()?.to_str().unwrap());
 
     let mut command = std::process::Command::new("ssh");
     command
-        .args(["localhost", "./bin/in", &pwd, "docker"])
-        .args(args)
+        // ssh -t allocates a tty so that ^C works correctly
+        .args(["-t", "localhost"])
+        // We assume that https://crates.io/crates/in-directory is installed already.
+        .args(["./bin/in", &pwd])
+        // Forward the rest of the docker args unchanged
+        .arg("docker")
+        .args(args);
+
+    // TODO: switch to pretty_env_logger or something
+    eprintln!("Running: {:?}", &command);
+
+    // It might be possible to `exec` here, but I'm keeping my options
+    // open, in case I need to do something with file synchronisation
+    // or port forwarding on demand, in the background.
+    command
         .stdin(std::process::Stdio::inherit())
         .stdout(std::process::Stdio::inherit())
         .stderr(std::process::Stdio::inherit());
-    dbg!(&command);
     let mut process = command.spawn()?;
     process.wait()?;
 
